8566236364b2a2ad1f609b415c5e4eccb0985f32
Emily Mo <46546633+moemily123819@users.noreply.github.com>
2019-04-13 08:05:58 -0400

    Update wine_database.sql

--- wine_database.sql
+++ wine_database.sql
@@ -2,4 +2,20 @@ CREATE DATABASE wine_db;
 use wine_db;
 CREATE TABLE wine_collection (
 id INT Primary Key,
+wine text,
+location text,
+score text,
+GWS_score text,
+nbj text,
+);
 
+
+
+CREATE TABLE top_20_wine (
+id INT PRIMARY KEY,
+wine text,
+vintage text,
+location text,
+score text,
+bottles_in_stock INT
+);
